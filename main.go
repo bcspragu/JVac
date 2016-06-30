d9f8678cfb7db59724df091cd6d2ef9ae2f88db1
Brandon Sprague <brandon00sprague@gmail.com>
2016-06-29 22:25:42 -0700

    Individual uplaods

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
-var jsonUploads []template.HTML
+type jsonUpload struct {
+	uploadTime time.Time
+	contents   []byte
+}
 
-var templates = template.Must(template.ParseGlob("*.html"))
+var jsonUploads []jsonUpload
+
+var templates = template.Must(template.New("").Funcs(template.FuncMap{
+	"raw": func(ju jsonUpload) template.HTML {
+		return template.HTML(string(ju.contents))
+	},
+}).ParseGlob("*.html"))
 
 func main() {
-	jsonUploads = []template.HTML{}
+	jsonUploads = []jsonUpload{}
 
 	http.HandleFunc("/", serveJSON)
 
@@ -23,6 +37,22 @@ func main() {
 }
 
 func serveJSON(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request to ", r.URL.Path)
+
+	if r.URL.Path != "/" {
+		s := strings.Split(r.URL.Path, "/")[1]
+		i, err := strconv.Atoi(s)
+
+		if i >= len(jsonUploads) || err != nil {
+			return
+		}
+
+		ju := jsonUploads[i]
+		rd := bytes.NewReader(ju.contents)
+		http.ServeContent(w, r, fmt.Sprintf("file%d.json", i), ju.uploadTime, rd)
+		return
+	}
+
 	switch r.Method {
 	case "POST":
 		uploadJSON(w, r)
@@ -38,7 +68,7 @@ func uploadJSON(w http.ResponseWriter, r *http.Request) {
 
 	if err := decoder.Decode(&u); err == nil {
 		if b, err := json.MarshalIndent(u, "", " "); err == nil {
-			jsonUploads = append(jsonUploads, template.HTML(string(b)))
+			jsonUploads = append(jsonUploads, jsonUpload{time.Now(), b})
 		} else {
 			log.Println("Error indenting json: ", err)
 		}
